2163e0865284cd4725f7193794d977ea5dfdc222
Kiran Mahmood <kiranmah22@gmail.com>
2019-06-30 03:36:05 -0700

    third draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,61 @@
-alert("hi")
+alert("Press any key to begin.")
 
-//1. User starts by clicking a letter from the keyboard. 
-//2. The letter shows up on the screen. 
-//3. The computer chooses a letter. 
-//4. If the computer and user match, the user wins. 
-//5. If the computer and user do not match, the user loses. 
-//6. The user has 9 tries to correctly match the computer.
-
-var wins = 0,
-    loss = 0,
-    guessLeft = 9,
-    guessedSoFar = 0;
+//Array of possible computer choices
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
+//Variables for tracking players wins, losses, & guesses left
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var lettersGuessed = [];
+var computerGuess = [];
 
+
+//Initial computer letter
+window.onload = function () {
+    var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess.push(compGuess);
+    console.log(computerGuess[0]);
+}
+
+
+//Game
 document.onkeyup = function (event) {
-    var userGuess = event.key;
+    var playerGuess = event.key;
+    lettersGuessed.push(playerGuess);
+    console.log(computerGuess[0]);
 
-    var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    if ((playerGuess === computerGuess[0]) && (guessesLeft > 0)) {
+        wins++;
+        guessesLeft = 9;
+        lettersGuessed.length = 0;
+        computerGuess.length = 0;
+        var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        computerGuess.push(compGuess);
+        console.log(computerGuess[0]);
+    }
 
-    var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
+    else if ((playerGuess !== computerGuess[0]) && (guessesLeft > 0)) {
+        guessesLeft = guessesLeft - 1;
+    }
 
-    //a simple way to look at the above function: 
-    //var computerGuess = letterChoices[]
+    else {
+        losses++;
+        guessesLeft = 9;
+        lettersGuessed.length = 0;
+        computerGuess.length = 0;
+        var compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        computerGuess.push(compGuess);
+        console.log(computerGuess[0]);
+    }
 
+    var html = "<p>Guess what letter I'm thinking of!</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses left: " + guessesLeft + "</p>" +
+        "<p>Your guesses so far: " + lettersGuessed + "</p>";
 
-
+    document.querySelector("#game").innerHTML = html;
 
 }
