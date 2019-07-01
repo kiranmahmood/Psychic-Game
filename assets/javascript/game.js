a4e278cb3a0a71ec258301fe3d04d56b15d2c485
kiranmahmood <51061834+kiranmahmood@users.noreply.github.com>
2019-06-30 23:01:14 -0700

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ alert("Press any key to begin.")
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-
+var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var lettersGuessed = [];
